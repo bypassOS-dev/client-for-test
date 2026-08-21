e23a028e803ea3961a8fd58acc0c11744744876a
bypassOS <265340571+bypassOS-dev@users.noreply.github.com>
2026-08-22 00:00:32 +0500

    Update fragmenting.rs

--- src/fragmenting.rs
+++ src/fragmenting.rs
@@ -21,3 +21,23 @@ impl AsyncRead for FragmentingStream {
         Pin::new(&mut this.inner).poll_read(cx, buf)
     }
 }
+impl AsyncWrite for FragmentingStream {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<std::io::Result<usize>> {
+        let this = self.get_mut();
+ 
+        if !this.first_write_done && buf.len() > 1 {
+            this.first_write_done = true;
+ 
+            let half = buf.len() / 2;
+            let first_part = &buf[..half];
+ 
+            Pin::new(&mut this.inner).poll_write(cx, first_part)
+        } else {
+            Pin::new(&mut this.inner).poll_write(cx, buf)
+        }
+    }
+}
