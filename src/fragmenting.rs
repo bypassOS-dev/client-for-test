570424e8adbafe7b17dfaecf89e4c263bcdb4517
bypassOS <265340571+bypassOS-dev@users.noreply.github.com>
2026-08-21 20:06:21 +0500

    Update fragmenting.rs

--- src/fragmenting.rs
+++ src/fragmenting.rs
@@ -1,5 +1,7 @@
 use tokio::net::TcpStream;
-
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
 pub struct FragmentingStream {
     inner: TcpStream,
     first_write_done: bool,
@@ -7,5 +9,14 @@ pub struct FragmentingStream {
 impl FragmentingStream {
     pub fn new(inner: TcpStream) -> Self {
         Self{inner, first_write_done: false}
+    }
+}
+impl AsyncRead for FragmentingStream {
+    fn poll_read(
+        self: Pin<&mut self>, 
+        cx: Context<'_>, 
+        buf: &[u8]
+    ) -> Poll<std::io::Result<usize>>{
+        todo!()
     }
 }
